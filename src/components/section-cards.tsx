d67e8d18442f417325e10a3c1200179d5258acf2
Thiago <thgps01@outlook.com>
2025-08-06 21:22:10 -0300

    fix: prettier code

--- src/components/section-cards.tsx
+++ src/components/section-cards.tsx
@@ -2,20 +2,15 @@
 
 import { financialIndicators } from '@/constants/financial-indicators'
 import { ManageIndicators } from './manage-indicators'
-import { useState } from 'react'
 import { KpiCard } from './kpi-card/kpi-card'
 
 export function SectionCards() {
-
-
   return (
     <div className="flex flex-col gap-4 border-b px-4 pb-4 lg:px-6 lg:pb-6">
       <div className="flex items-center justify-between">
         <div className="text-xl font-semibold">Indicadores Financeiros</div>
 
-        <ManageIndicators
-
-        />
+        <ManageIndicators />
       </div>
       <div className="grid grid-cols-1 gap-4 @2xl/main:grid-cols-2 @6xl/main:grid-cols-4">
         {financialIndicators.map((item) => (
